7e0c4a49c4ff6a946dd700de767111a6acdbaa66
Wenjie Hu <wenjie.hoo@outlook.com>
2024-07-01 21:14:04 +0200

    main

---
+++ themes/async/scripts/filters/index.js
@@ -0,0 +1,17 @@
+hexo.extend.filter.register('after_post_render', data => {
+	let replaceText = ` data-tag='post-image'`;
+
+	// img lazyload
+	if (hexo.theme.config.lazyload && hexo.theme.config.lazyload.enable) {
+		replaceText += ` loading="lazy"`;
+	}
+
+	// img failed to load
+	if (hexo.theme.config.error_img.post_page) {
+		replaceText += ` ${hexo.extend.helper.get('onerror').bind(hexo)('post_page')}`;
+	}
+
+	data.content = data.content.replace(/<img(.+?)(\/{0,1})>/gi, (str, p1) => str.replace(p1, `${p1} ${replaceText}`));
+
+	return data;
+});
